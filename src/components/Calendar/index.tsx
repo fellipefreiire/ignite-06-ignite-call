e6cc30e978140c3a9c55a40eb56e510621d66ec8
Felipe <fellipefreiire3@gmail.com>
2023-03-09 20:51:19 -0300

    calendar component

---
+++ src/components/Calendar/index.tsx
@@ -0,0 +1,53 @@
+import { CaretLeft, CaretRight } from 'phosphor-react'
+import * as S from './styles'
+import { getWeekDays } from '@/utils/get-week-days'
+
+export function Calendar() {
+  const shortWeekDays = getWeekDays({ short: true })
+
+  return (
+    <S.CalendarContainer>
+      <S.CalendarHeader>
+        <S.CalendarTitle>
+          Março <span>2023</span>
+        </S.CalendarTitle>
+
+        <S.CalendarActions>
+          <button>
+            <CaretLeft />
+          </button>
+          <button>
+            <CaretRight />
+          </button>
+        </S.CalendarActions>
+      </S.CalendarHeader>
+
+      <S.CalendarBody>
+        <thead>
+          <tr>
+            {shortWeekDays.map((weekDay) => (
+              <th key={weekDay}>{weekDay}.</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+              <S.CalendarDay>1</S.CalendarDay>
+            </td>
+            <td>
+              <S.CalendarDay>2</S.CalendarDay>
+            </td>
+            <td>
+              <S.CalendarDay>3</S.CalendarDay>
+            </td>
+          </tr>
+        </tbody>
+      </S.CalendarBody>
+    </S.CalendarContainer>
+  )
+}
