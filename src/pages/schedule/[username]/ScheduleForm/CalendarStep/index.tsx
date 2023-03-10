f065ffe6d395ab6671abf1a07d1dd1a770ac3ee6
Felipe <fellipefreiire3@gmail.com>
2023-03-10 16:34:08 -0300

    confirm page styles

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -2,9 +2,37 @@ import { Calendar } from '@/components/Calendar'
 import * as S from './styles'
 
 export function CalendarStep() {
+  const isDateSelected = true
+
   return (
-    <S.Container>
+    <S.Container isTimePickerOpen={isDateSelected}>
       <Calendar />
+
+      {isDateSelected && (
+        <S.TimePicker>
+          <S.TimePickerHeader>
+            terça-feira <span>20 de setembro</span>
+          </S.TimePickerHeader>
+
+          <S.TimePickerList>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+          </S.TimePickerList>
+        </S.TimePicker>
+      )}
     </S.Container>
   )
 }
