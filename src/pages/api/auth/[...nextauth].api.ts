3562fbeed2b682fd649bdfd20f8d2528ac563925
Felipe <fellipefreiire3@gmail.com>
2023-03-13 19:22:04 -0300

    google refresh token

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -17,6 +17,9 @@ export function buildNextAuthOptions(
           params: {
             scope:
               'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
+            prompt: 'consent',
+            response_type: 'code',
+            access_type: 'offline',
           },
         },
         profile(profile: GoogleProfile) {
